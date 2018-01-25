5378c1d6ca23228255159ff05cadb3190ec45166
Kristy Leung <kristy.leung11@gmail.com>
2018-01-25 02:34:46 -0800

    d
    jnl

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 var typed = new Typed('.element', {
-  strings: ["watching Dad use Facebook", "extreme knitting","Larry David",  "r/natureismetal", "true crime podcasts", "shipwrecks","Toonie Tuesdays"],
+  strings: ["watching Dad use Facebook", "stand up",  "r/natureismetal", "true crime podcasts", "shipwrecks"],
   typeSpeed: 70,
   backDelay: 1500,
   loop: true
