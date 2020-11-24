dbe9cd8a95674e5c07855f4ea258ceae1d39ddf3
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-11-24 21:53:16 +0100

    chore: prettier

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,8 +30,8 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`
-      }
+        path: `${__dirname}/src/pages`,
+      },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
