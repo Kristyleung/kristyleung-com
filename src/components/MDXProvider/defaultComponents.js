cdd9acdb154ca0ca7a217bbacac3edec345fc05f
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-11-25 18:09:24 +0100

    feat: implement useTheme

--- src/components/MDXProvider/defaultComponents.js
+++ src/components/MDXProvider/defaultComponents.js
@@ -1,67 +1,93 @@
 import React from 'react'
-import { css } from '@emotion/react'
+import { useTheme, css } from '@emotion/react'
 
-const h1Style = css({
-  fontFamily: 'Public Sans',
+const h1Style = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
 })
 
-const h2Style = css({
-  fontFamily: 'Public Sans',
+const h2Style = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
 })
 
-const h3Style = css({
-  fontFamily: 'Public Sans',
+const h3Style = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
 })
 
-const linkStyle = css({
-  fontFamily: 'Public Sans',
+const linkStyle = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
 })
 
-const ulStyle = css({})
+const ulStyle = ({ theme }) => ({})
 
-const olStyle = css({})
+const olStyle = ({ theme }) => ({})
 
-const liStyle = css({
-  fontFamily: 'Public Sans',
+const liStyle = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
   lineHeight: 1.5,
 })
 
-const paragraphStyle = css({
-  fontFamily: 'Public Sans',
+const paragraphStyle = ({ theme }) => ({
+  fontFamily: theme.font.family.text,
+  fontSize: theme.font.size[3],
   lineHeight: 1.5,
+  color: theme.colors.black,
 })
 
-const H1 = props => (
-  <h1 css={h1Style} {...props}>
-    {props.children}
-  </h1>
-)
+const H1 = props => {
+  const theme = useTheme()
+  return (
+    <h1 css={h1Style({ theme })} {...props}>
+      {props.children}
+    </h1>
+  )
+}
 
-const H2 = props => (
-  <h2 css={h2Style} {...props}>
-    {props.children}
-  </h2>
-)
+const H2 = props => {
+  const theme = useTheme()
+  return (
+    <h2 css={h2Style({ theme })} {...props}>
+      {props.children}
+    </h2>
+  )
+}
 
-const H3 = props => (
-  <h3 css={h3Style} {...props}>
-    {props.children}
-  </h3>
-)
+const H3 = props => {
+  const theme = useTheme()
+  return (
+    <h3 css={h3Style({ theme })} {...props}>
+      {props.children}
+    </h3>
+  )
+}
 
-const Ul = props => <ul css={ulStyle} {...props} />
+const Ul = props => {
+  const theme = useTheme()
+  return <ul css={ulStyle({ theme })} {...props} />
+}
 
-const Ol = props => <ol css={olStyle} {...props} />
+const Ol = props => {
+  const theme = useTheme()
+  return <ol css={olStyle({ theme })} {...props} />
+}
 
-const Li = props => <li css={liStyle} {...props} />
+const Li = props => {
+  const theme = useTheme()
+  return <li css={liStyle({ theme })} {...props} />
+}
 
-const Link = props => (
-  <a css={linkStyle} {...props}>
-    {props.children}
-  </a>
-)
+const Link = props => {
+  const theme = useTheme()
+  return (
+    <a css={linkStyle({ theme })} {...props}>
+      {props.children}
+    </a>
+  )
+}
 
-const Paragraph = props => <p css={paragraphStyle} {...props} />
+const Paragraph = props => {
+  const theme = useTheme()
+  return <p css={paragraphStyle({ theme })} {...props} />
+}
 
 const components = {
   wrapper: function Wrapper({ children, ...props }) {
--- src/utils/theme.js
+++ src/utils/theme.js
@@ -1,7 +1,14 @@
 const theme = {
+  font: {
+    family: {
+      text: 'Public Sans',
+    },
+    size: [8, 12, 16, 18, 24, 32, 64],
+  },
   colors: {
     white: '#FFF',
     black: '#111',
+    primary: 'blue',
   },
   spacing: [4, 8, 16, 24, 32, 64],
 }
