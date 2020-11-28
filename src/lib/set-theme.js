bf9b2cf0b2e3915fe5c7923dee17c41871e7a893
Thijs Krooswijk <t.krooswijk@gmail.com>
2020-11-28 01:14:07 +0100

    fix: check ssr

--- src/lib/set-theme.js
+++ src/lib/set-theme.js
@@ -1,15 +1,14 @@
-
 import { isBrowser } from './is-browser'
 import { light, dark } from '../theming/theme'
 
-export const setTheme = (theme) => {
-    let matchColorScheme = false
-    /** Check localStorage */
-    if(localStorage.getItem('theme'))
-        matchColorScheme = localStorage.getItem('theme')
-    /** Check browser environment */
-    if(isBrowser) 
-        matchColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
-        localStorage.setItem('theme', 'dark')
-    return theme = matchColorScheme ? dark : light
+export const setTheme = theme => {
+  let matchColorScheme = false
+  /** Check browser environment */
+  if (isBrowser)
+    if (localStorage.getItem('theme'))
+      /** Check localStorage */
+      matchColorScheme = localStorage.getItem('theme')
+    else matchColorScheme = window.matchMedia('(prefers-color-scheme: dark)').matches
+  localStorage.setItem('theme', 'dark')
+  return (theme = matchColorScheme ? dark : light)
 }
