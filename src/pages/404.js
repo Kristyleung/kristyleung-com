d79b0f8ff29025c71c3d309a2953c0168903055d
Thijs Krooswijk <t.krooswijk@gmail.com>
2021-03-04 22:50:47 +0100

    chore: prettier

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,7 +7,7 @@ const NotFoundPage = () => (
     title="404: Not found"
     description="You just hit a route that doesn't exist... the sadness."
     keywords={['404', 'Not found']}
-    location={{ pathname: "/404" }}
+    location={{ pathname: '/404' }}
   >
     <h1>404: Not Found</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
